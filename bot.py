4f4d744af9ebed9896755838383e1ea0da6bc4a5
nawejk <naweedpay@web.de>
2025-11-04 08:08:48 +0100

    bot.py aktualisieren

--- bot.py
+++ bot.py
@@ -1182,7 +1182,7 @@ def cmd_start(m: Message):
     u = get_user(uid)
     bot.reply_to(m, home_text(u), reply_markup=kb_main(u))
 
-@@bot.callback_query_handler(func=lambda c: (c.data or "").startswith(("subs_", "admin_subs")))
+@bot.callback_query_handler(func=lambda c: (c.data or "").startswith(("subs_", "admin_subs")))
 def on_cb_subs(c: CallbackQuery):
     uid = c.from_user.id
     data = c.data or ""
